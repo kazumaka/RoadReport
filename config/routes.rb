5d962334f635bb33b528686de868464b6065c730
kazumaka <kw1997.10.15@softbank.ne.jp>
2022-04-18 13:27:31 +0000

    ログイン機能実装完了

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+ devise_for :users,skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
+
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+}
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  scope module: :public do
+    root to: 'homes#top'
+  end
+end
