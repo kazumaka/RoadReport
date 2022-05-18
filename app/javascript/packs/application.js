0926d8102eed99765f6930dc988abe6770ba1fc7
kazumaka <kw1997.10.15@softbank.ne.jp>
2022-05-18 11:58:59 +0000

    全体地図5

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,7 +6,7 @@ require("../src/underscore")
 require("../src/googlemap")
 
 import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
+// import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
